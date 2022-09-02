3bbdaa67d68432ab3a9d15898b0a7d021f420c52
Adrian Pope <apope@anl.gov>
2022-09-02 17:38:22 -0500

    bcast tarball to all nodes in job even if not going to use for mpi

--- startup_theta_local/qsub_theta.sh
+++ startup_theta_local/qsub_theta.sh
@@ -11,12 +11,14 @@ RPN=$2
 OUTBASE=$3
 CMD_STR=$4
 
+NODES_JOB=${COBALT_JOBSIZE}
+
 RANKS=$((${NODES_RUN}*${RPN}))
 
 OUTNAME=${OUTBASE}.csv
 LOGNAME=${OUTBASE}.log
 
 echo -n "${NODES_RUN}, ${RPN}, " > ${OUTNAME}
-/usr/bin/time -f "%e" aprun -n ${NODES_RUN} -N 1 -d 1 -j 1 -cc depth ./conda-unpack.sh ${LOGNAME} 2>&1 >/dev/null | tr -d '\n' >> ${OUTNAME}
+/usr/bin/time -f "%e" aprun -n ${NODES_JOB} -N 1 -d 1 -j 1 -cc depth ./conda-unpack.sh ${LOGNAME} 2>&1 >/dev/null | tr -d '\n' >> ${OUTNAME}
 echo -n ", " >> ${OUTNAME}
 /usr/bin/time -f "%e" aprun -n ${RANKS} -N ${RPN} -d 1 -j 1 -cc depth ${CMD_STR} 2>> ${OUTNAME}
