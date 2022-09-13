ba4e8eda26e36ecf75a1e34f8b8e1d035358e75d
Adrian Pope <apope@anl.gov>
2022-09-12 19:19:13 -1000

    modified input data paths in plot scripts

--- startup/plots/plot_conda.sh
+++ startup/plots/plot_conda.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 python3 ./plot.py conda "conda" \
-	../startup_theta_grand/conda_01.csv "Theta/grand (1 RPN)" \
-	../startup_theta_grand/conda_64.csv "Theta/grand (64 RPN)" \
-	../startup_cori-knl_common/conda_01.csv "Cori-KNL/common (1 RPN)" \
-	../startup_cori-knl_common/conda_68.csv "Cori-KNL/common (68 RPN)"
+	../theta_grand/conda_01.csv "Theta/grand (1 RPN)" \
+	../theta_grand/conda_64.csv "Theta/grand (64 RPN)" \
+	../cori-knl_common/conda_01.csv "Cori-KNL/common (1 RPN)" \
+	../cori-knl_common/conda_68.csv "Cori-KNL/common (68 RPN)"
--- startup/plots/plot_numpy.sh
+++ startup/plots/plot_numpy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 python3 ./plot.py numpy "import numpy" \
-	../startup_theta_grand/numpy_01.csv "Theta/grand (1 RPN)" \
-	../startup_theta_grand/numpy_64.csv "Theta/grand (64 RPN)" \
-	../startup_cori-knl_common/numpy_01.csv "Cori-KNL/common (1 RPN)" \
-	../startup_cori-knl_common/numpy_68.csv "Cori-KNL/common (68 RPN)" \
-	../startup_theta_local/numpy_01.csv "Theta/SSD (1 RPN)" \
-	../startup_theta_local/numpy_64.csv "Theta/SSD (64 RPN)"
+	../theta_grand/numpy_01.csv "Theta/grand (1 RPN)" \
+	../theta_grand/numpy_64.csv "Theta/grand (64 RPN)" \
+	../cori-knl_common/numpy_01.csv "Cori-KNL/common (1 RPN)" \
+	../cori-knl_common/numpy_68.csv "Cori-KNL/common (68 RPN)" \
+	../theta_local/numpy_01.csv "Theta/SSD (1 RPN)" \
+	../theta_local/numpy_64.csv "Theta/SSD (64 RPN)"
